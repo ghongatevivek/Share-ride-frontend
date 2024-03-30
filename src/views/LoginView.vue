baf21a30baa30112e9e626b945a31e4d2ab66bde
ghongatevivek <ghongatevivek1212@gmail.com>
2024-03-31 00:34:07 +0530

    Working on frontend part

---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
+        <form>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <input type="text" v-maska data-maska="###-###-####" name="phone" id="phone" class="appearance-none mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
+                    </div>
+                </div>
+
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button type="submit" class="inline-flex justify-center items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-black hover:bg-gray-700 focus:outline-none focus:border-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-50 transition ease-in-out duration-150">
+                        Continue
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+    import {vMaska} from 'Maska'
+</script>
